7cf43323c62741a2a1544fbcfbbf53435d34adee
arhanashik <ashik.pstu.cse@gmail.com>
2021-11-25 10:10:53 +0900

    Initial Commit

---
+++ PSTUian/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PSTUian
+//
+//  Created by Arhan Ashik on 2021/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PSTUian/PSTUianApp.swift
@@ -0,0 +1,17 @@
+//
+//  PSTUianApp.swift
+//  PSTUian
+//
+//  Created by Arhan Ashik on 2021/11/25.
+//
+
+import SwiftUI
+
+@main
+struct PSTUianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
